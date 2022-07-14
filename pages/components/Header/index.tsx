5cf9e0153ff61eb9323f36fbd19eac4430fbe2e0
hsggamer <55681300+hsggamer@users.noreply.github.com>
2022-07-14 11:41:04 +0400

    Commit

---
+++ pages/components/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+    <div className="w-[1440px] h-[93px] left-0 top-0 absolute bg-[#ffffff]">
+        <h1 className="Logo">Dealerz.</h1>
+    </div>
+    )
+
+}
+
+export default Header;
