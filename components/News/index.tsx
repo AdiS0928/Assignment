412bf08211eef37f8c605612a470398444f4a237
hsggamer <55681300+hsggamer@users.noreply.github.com>
2022-07-16 18:05:07 +0400

    c

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -6,7 +6,7 @@ const News = () =>
     	<p className="newsdesc ml-[45vh] mt-[35vh]">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster </p>
         <div className="w-[816px] h-[64px] absolute ml-[30vh] mt-[45vh] bg-slate-100 hover:cursor-text">
             <p className="newstext mt-[3.5vh] ml-[5vh]">Insert your mail here</p>
-            <button className="ml-[93vh] mt-[1.5vh]"><img src="/newsbut.png" alt="" className="newsbut" /></button>
+            <button className="ml-[100vh] mt-[1.5vh]"><img src="/newsbut.png" alt="" className="newsbut" /></button>
         </div>
 </div>
     )
